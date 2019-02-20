68ab8e387c68b634d60735c5e98877dce7fa8b3f
Florian Inchauspe <florian.inchauspe-papi@iut-rodez.fr>
2019-02-20 14:13:27 +0100

    Test

--- src/main/resources/db/migration/R__Function_to_add_a_new_activite.sql
+++ src/main/resources/db/migration/R__Function_to_add_a_new_activite.sql
@@ -1,3 +1,3 @@
 create or replace function add_activity_with_title(title varchar(200)) returns bigint as $$
     insert into activity (id, title) values (nextval('id_generator'),add_activity_with_title.title) returning id;
-$$
+$$ language sql;
